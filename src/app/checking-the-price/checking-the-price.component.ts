768d6d3c57da1be383e9ed9fe9062f844ccf935f
RoutinieMusell02 e <mateusz.maslanka1@outlook.com>
2021-02-28 15:42:23 +0100

    add update rates

--- src/app/checking-the-price/checking-the-price.component.ts
+++ src/app/checking-the-price/checking-the-price.component.ts
@@ -1,17 +1,17 @@
-import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {GetCurrencyService} from '../currency-exchange/Services/get-currency.service';
-import {first, map, mergeMap, pluck, skipWhile, switchMap, take, takeWhile, tap} from 'rxjs/operators';
+import {map, mergeMap, pluck, takeWhile} from 'rxjs/operators';
 import {Curriencies} from '../currency-exchange/Model/currencies';
-import {fromEvent, interval, Observable, Subscription} from 'rxjs';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-checking-the-price',
   templateUrl: './checking-the-price.component.html',
   styleUrls: ['./checking-the-price.component.scss']
 })
-export class CheckingThePriceComponent implements AfterViewInit {
+export class CheckingThePriceComponent implements OnInit, OnDestroy {
 
-  constructor(private getCurrencyService: GetCurrencyService) { }
+  constructor(private getCurrencyService: GetCurrencyService)  { }
 
   getRates: {[k: string]: number} = {};
   updateTime = 1;
@@ -19,18 +19,23 @@ export class CheckingThePriceComponent implements AfterViewInit {
   getCurrency: string;
   isStart = false;
   interval: Subscription;
-  @ViewChild('start', { static: true }) startButton: ElementRef;
 
-  ngAfterViewInit() {
+  ngOnInit() {
     (Object.keys(this.getCurrencyService.getCourses()).length === 0) ? this.getRatesFormApi('PLN').subscribe(el => this.getRates = el)
     : this.getRates = this.getCurrencyService.getCourses();
+  }
+
+  ngOnDestroy() {
+    if (this.isStart) {
+      this.interval.unsubscribe();
+    }
   }
 
   getRatesFormApi(getCurrencies) {
     return this.getCurrencyService.currenciesToCalculate(getCurrencies).pipe(
       map((el: Curriencies) => {
         return el.rates;
-      }));
+    }));
   }
 
   getCurrencyValue(currency) {
@@ -45,11 +50,10 @@ export class CheckingThePriceComponent implements AfterViewInit {
     }
     if (this.updateTime >= 1 && this.updateTime < 30) {
       this.isStart = true;
-      this.interval = interval(time).pipe(
-        tap(() => this.isStart = true),
+      this.interval = interval(3000).pipe(
         mergeMap(() => this.getRatesFormApi(this.getCurrency)),
         pluck('PLN'),
-        takeWhile(el => el <= this.getPrice, true)
+        takeWhile(el => el >= this.getPrice, true)
       ).subscribe(el => console.log(el));
     }
   }
